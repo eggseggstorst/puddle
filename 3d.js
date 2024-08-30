3c90e7d5ef5b6d5dd9adf8264d58d694f3a44815
eggseggstorst <126527922+eggseggstorst@users.noreply.github.com>
2024-08-30 12:17:13 +1000

    Update 3d.js

--- 3d.js
+++ 3d.js
@@ -1,4 +1,4 @@
-var scene, camera, renderer, controls;
+// var scene, camera, renderer, controls;
 var container;
 var loader;
 var w = window.innerWidth;
@@ -140,7 +140,7 @@ function initCanvasTex(){
 	image.onload = function (){
 		ctx.drawImage(image, 0, 0);
 	}
-	image.src = "../img/rainbow.jpg";
+	image.src = "../img/check.jpg";
 
     tex = new THREE.Texture(canvasRenderer.domElement);
     // tex = new THREE.Texture(canvas);
